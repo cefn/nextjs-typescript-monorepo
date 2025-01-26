4632248d0e31e71520cc2b441c35de5ced08dcfd
Cefn Hoile <choile@roku.com>
2025-01-26 15:35:52 +0000

    Add fixes as clean commit

--- servers/nextjs-with-dependencies/next.config.ts
+++ servers/nextjs-with-dependencies/next.config.ts
@@ -1,7 +1,31 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, _options) => {
+    const packagesPath = path.resolve(__dirname, "../../packages");
+
+    config.resolve.extensions.push(".ts", ".tsx");
+
+    const localAliases = Object.fromEntries(
+      ["sum", "multiply"].map(
+        (name) =>
+          [
+            `@myrepo/${name}`,
+            path.resolve(packagesPath, `${name}/src`),
+          ] as const,
+      ),
+    );
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      ...localAliases,
+    };
+
+    config.resolve.conditionNames = ["@myrepo", "import", "require", "default"];
+
+    return config;
+  },
 };
 
 export default nextConfig;
