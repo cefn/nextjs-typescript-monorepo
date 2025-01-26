64e9e48eaaf38c0738848cd57435cdc82e0104c4
Cefn Hoile <choile@roku.com>
2025-01-26 15:34:48 +0000

    Reset to build without live types

--- servers/nextjs-with-dependencies/next.config.ts
+++ servers/nextjs-with-dependencies/next.config.ts
@@ -1,31 +1,7 @@
-import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, _options) => {
-    const packagesPath = path.resolve(__dirname, "../../packages");
-
-    config.resolve.extensions.push(".ts", ".tsx");
-
-    const localAliases = Object.fromEntries(
-      ["sum", "multiply"].map(
-        (name) =>
-          [
-            `@myrepo/${name}`,
-            path.resolve(packagesPath, `${name}/src`),
-          ] as const,
-      ),
-    );
-
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      ...localAliases,
-    };
-
-    config.resolve.conditionNames = ["@myrepo", "import", "require", "default"];
-
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
